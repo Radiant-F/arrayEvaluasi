f0043810d3231ec149f719485f3633d2cd42f6d5
RadiantF-pondok <71006921+RadiantF-pondok@users.noreply.github.com>
2020-09-12 06:11:17 +0700

    nomor 1
    nomor 1

---
+++ Array/script.js
@@ -0,0 +1,7 @@
+var nilaiSantri = [50, 90, 65, 67, 89, 80, 50, 97, 84]
+var total = 0;
+
+for (i = 0; i < nilaiSantri.length; i++) {
+  total += nilaiSantri.length
+}
+document.write(total / nilaiSantri.length)
