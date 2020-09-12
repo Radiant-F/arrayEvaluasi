07f7a56ccd19cb0bbebad2e992e947907da27834
RadiantF-pondok <71006921+RadiantF-pondok@users.noreply.github.com>
2020-09-12 07:04:55 +0700

    upddateee
    reeee

--- Array/script.js
+++ Array/script.js
@@ -2,6 +2,6 @@ var nilaiSantri = [50, 90, 65, 67, 89, 80, 50, 97, 84]
 var total = 0;
 
 for (i = 0; i < nilaiSantri.length; i++) {
-  total += nilaiSantri.length
+  total += nilaiSantri[i]
 }
 document.write(total / nilaiSantri.length)
